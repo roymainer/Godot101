8a406e54f50d424ba98dd904b5928f4bf3b2309a
rmainer <roy.mainer@intel.com>
2018-12-19 17:32:49 +1000

    tutorial done

--- Main.gd
+++ Main.gd
@@ -2,6 +2,11 @@ extends Node
 
 onready var gem = preload("res://gem.tscn")  # preload the gem scene
 onready var gem_container = get_node("gem_container")  # preload the container (once)
+onready var score_label = get_node("HUD/score_label")  # preload the score label
+onready var time_label = get_node("HUD/time_label")  
+onready var game_over_label = get_node("HUD/game_over_label")
+onready var game_timer = get_node("game_timer")
+
 
 var screensize
 var level = 1
@@ -17,8 +22,11 @@ func _ready():
 	screensize = get_viewport().size
 	set_process(true)
 	spawn_gem(10)  # spawn 10 gems
+	game_over_label.hide()
+	
 	
 func _process(delta):
+	time_label.set_text(str(int(game_timer.get_time_left())))
 	if gem_container.get_child_count() == 0:
 		level += 1
 		spawn_gem(10 * level)
@@ -27,5 +35,15 @@ func spawn_gem(num):
 	for i in range(num):
 		var g = gem.instance()  # instance of a gem object
 		gem_container.add_child(g)  # store it inside the container
-		g.set_position(Vector2(rand_range(0, screensize.x - 40), rand_range(0, screensize.y - 40)))
+		g.connect("gem_grabbed", self, "_on_gem_grabbed")
+		var g_extents = g.get_node("sprite").get_texture().get_size() /2 
+		g.set_position(Vector2(rand_range(g_extents.x, screensize.x - g_extents.x), rand_range(g_extents.y, screensize.y - g_extents.y)))
 		
+func _on_gem_grabbed():
+	score += 10
+	score_label.set_text(str(score))
+#	print(score)
+
+func _on_game_timer_timeout():
+	get_node("player").set_physics_process(false)
+	game_over_label.show()
--- gem.gd
+++ gem.gd
@@ -1,9 +1,43 @@
 extends Area2D
 
+onready var fadeout_effect = get_node("fadeout_effect")
+#onready var fadein_effect = get_node("fadein_effect")
+onready var sprite = get_node("sprite")
+
+signal gem_grabbed
+
 func _ready():
-	pass
+	fadeout_effect.interpolate_property(sprite, 
+								"scale", 
+								sprite.get_scale(), 
+								Vector2(2.0, 2.0), 
+								0.3, 
+								Tween.TRANS_QUAD, 
+								Tween.EASE_OUT)
+	fadeout_effect.interpolate_property(sprite, 
+								"modulate", 
+								Color(1,1,1,1), 
+								Color(1,1,1,0), 
+								0.3, 
+								Tween.TRANS_QUAD, 
+								Tween.EASE_OUT)
+								
+#	fadein_effect.interpolate_property(sprite, "scale", Vector2(0,0), Vector2(1,1), 0.3, Tween.TRANS_QUAD, Tween.EASE_IN)
+#	fadein_effect.interpolate_property(sprite, "modulate", Color(1,1,1,0), Color(1,1,1,1), 0.3, Tween.TRANS_QUAD, Tween.EASE_IN)
+#
+#	fadein_effect.start()
+#	fadein_effect.set_repeat(false)
 
 func _on_gem_area_entered(area):
-	print(area.get_name())  # print what area overlaps
+#	print(area.get_name())  # print what area overlaps
 	if area.get_name() == "player":
-		queue_free()  # delete the node at the end of the frame
+		emit_signal("gem_grabbed")
+		
+		# get rid of the touched gem object so the player can't double touch for extra score
+		var owners = get_shape_owners()
+		shape_owner_clear_shapes(owners[0]) 
+		fadeout_effect.start()  # start the tween effect
+		
+
+func _on_effect_tween_completed(object, key):
+	queue_free()  # delete the node when the tween completes
